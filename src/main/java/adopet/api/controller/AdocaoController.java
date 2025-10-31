0284ff03ee9c9944d7ab1069dc466dcdc525e1eb
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-31 18:07:40 -0300

    Criando Exception customizada

--- src/main/java/adopet/api/controller/AdocaoController.java
+++ src/main/java/adopet/api/controller/AdocaoController.java
@@ -4,6 +4,7 @@ import adopet.api.dto.AdocaoDTO;
 import adopet.api.dto.AprovarAdocaoDTO;
 import adopet.api.dto.ReprovarAdocaoDTO;
 import adopet.api.dto.SolicitacaoDeAdocaoDTO;
+import adopet.api.exception.AdocaoException;
 import adopet.api.service.AdocaoService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
@@ -39,7 +40,7 @@ public class AdocaoController {
     public ResponseEntity<String> solicitar(@RequestBody @Valid SolicitacaoDeAdocaoDTO dados){
         try {
             this.service.solicitar(dados);
-        } catch (IllegalStateException | UnsupportedOperationException ex) {
+        } catch (AdocaoException ex) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
         }
 
---
+++ src/main/java/adopet/api/exception/AdocaoException.java
@@ -0,0 +1,9 @@
+package adopet.api.exception;
+
+public class AdocaoException extends RuntimeException {
+
+    public AdocaoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
