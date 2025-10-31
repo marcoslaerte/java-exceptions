f7eb41cce8191e3807e21225645bc979b2b2ffa8
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-31 20:07:33 -0300

    Retornando um ResponseError com informações mais detelhadas

--- src/main/java/adopet/api/exception/GlobalExceptionHandler.java
+++ src/main/java/adopet/api/exception/GlobalExceptionHandler.java
@@ -5,12 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AdocaoException.class)
-    public ResponseEntity<String> adocaoException(AdocaoException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ResponseError> adocaoException(AdocaoException ex) {
+
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
 }
